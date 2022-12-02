ecbb54cd1868cf2fce089dd8aa70116b2d299d89
Zia123456 <m.ziabakhteyari12@gmail.com>
2022-12-02 07:51:46 -0800

    events added

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,189 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import '../App.css';
+import calculate from '../logic/calculate';
+import '../logic/operate';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+      err: null,
+    };
+    this.clickHandler = (e) => {
+      this.setState((prevState) => calculate(prevState, e.target.name));
+    };
+  }
+
   render() {
     return (
       <div className="main-container">
         <div className="calculation-field">
-          <input type="number" />
-          <h1 className="answer">0</h1>
+          <h1 className="answer">
+            {this.state.next ||
+              this.state.operation ||
+              this.state.total ||
+              this.state.err ||
+              0}
+          </h1>
           <div className="numbers-and-symbols">
-            <div className="AC size">AC</div>
-            <div className="plus-or-minus size">+/-</div>
-            <div className="percentage size">%</div>
-            <div className="division size orabg-bg">/</div>
-            <div className="seven size">7</div>
-            <div className="eight size">8</div>
-            <div className="nine size">9</div>
-            <div className="cross size orabg-bg">x</div>
-            <div className="four size">4</div>
-            <div className="five size">5</div>
-            <div className="six size">6</div>
-            <div className="minus size orabg-bg">-</div>
-            <div className="one size">1</div>
-            <div className="two size">2</div>
-            <div className="three size">3</div>
-            <div className="plus size orabg-bg">+</div>
-            <div className="zero">0</div>
-            <div className="dot size">.</div>
-            <div className="equal size orabg-bg">=</div>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="AC size"
+              name="AC"
+            >
+              AC
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="plus-or-minus size"
+              name="+/-"
+            >
+              +/-
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="percentage size"
+              name="%"
+            >
+              %
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="division size orabg-bg"
+              name="÷"
+            >
+              ÷
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="seven size"
+              name="7"
+            >
+              7
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="eight size"
+              name="8"
+            >
+              8
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="nine size"
+              name="9"
+            >
+              9
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="cross size orabg-bg"
+              name="x"
+            >
+              x
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="four size"
+              name="4"
+            >
+              4
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="five size"
+              name="5"
+            >
+              5
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="six size"
+              name="6"
+            >
+              6
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="minus size orabg-bg"
+              name="-"
+            >
+              -
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="one size"
+              name="1"
+            >
+              1
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="two size"
+              name="2"
+            >
+              2
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="three size"
+              name="3"
+            >
+              3
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="plus size orabg-bg"
+              name="+"
+            >
+              +
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="zero"
+              name="0"
+            >
+              0
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="dot size"
+              name="."
+            >
+              .
+            </button>
+            <button
+              type="button"
+              onClick={this.clickHandler}
+              className="equal size orabg-bg"
+              name="="
+            >
+              =
+            </button>
           </div>
         </div>
       </div>
